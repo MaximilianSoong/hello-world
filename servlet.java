8097b1c0a1dc36d57102b141d7433f42eff53da6
宋增铭 <91167637+songzengming@users.noreply.github.com>
2024-04-28 12:44:11 +0800

    Update servlet.java

--- servlet.java
+++ servlet.java
@@ -1,1 +1,15 @@
-
+// WebServlet注解
+@WebServlet(urlPatterns = "/")
+public class HelloServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        // 设置响应类型:
+        resp.setContentType("text/html");
+        // 获取输出流:
+        PrintWriter pw = resp.getWriter();
+        // 写入响应:
+        pw.write("<h1>Hello, world!</h1>");
+        // flush强制输出:
+        pw.flush();
+    }
+}
